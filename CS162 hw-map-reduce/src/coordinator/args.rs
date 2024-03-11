c3fe2b82e8a80f55c3ad4e93b6667acca10c15f9
Code Help Tutor <1823890830@qq.com>
2024-03-11 14:24:04 +0000

    WeChat cstutorcs CS162

---
+++ CS162 hw-map-reduce/src/coordinator/args.rs
@@ -0,0 +1,11 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+use clap::Parser;
+
+/// A MapReduce coordinator
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    // The coordinator does not take any arguments
+}
